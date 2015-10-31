cfd09b591a503354bf9679367cea483c6aaa1dac
Aurélien Gâteau <mail@agateau.com>
2015-10-31 23:52:38 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+version = '1.0'
+
+setup(name='nanopod',
+      version=version,
+      description='Minimalist podcast downloader',
+      author='Aurélien Gâteau',
+      author_email='mail@agateau.com',
+      scripts=['nanopod']
+)
